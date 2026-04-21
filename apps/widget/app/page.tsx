59b1a798d3a67d1cb58787a9f5eb56ca41f518f3
Harsh Shukla <108127262+harshshukla2807@users.noreply.github.com>
2026-04-21 18:05:14 +0530

    02: convex package

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,23 +1,21 @@
-import { Button } from "@workspace/ui/components/button"
-import {add } from "@workspace/math/add"
-import { Input } from "@workspace/ui/components/input"
+"use client"
+
+import { useMutation, useQuery } from 'convex/react'
+import { api } from '@workspace/backend/_generated/api'
+import { Button } from '@workspace/ui/components/button'
 
 export default function Page() {
+  
+  const users = useQuery(api.users.getMany)
+  const addUser = useMutation(api.users.add)
+
   return (
-    <div className="flex min-h-svh p-6">
-      <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
-        <div>
-          <h1 className="font-medium">Project ready!</h1>
-          <p>You may now add components and start building.</p>
-          <p>We&apos;ve already added the button component for you.</p>
-          <Button className="mt-2">Button</Button>
-        </div>
-        <div className="text-muted-foreground font-mono text-xs">
-          (Press <kbd>d</kbd> to toggle dark mode)
-          <p>{add(1, 2)}</p>
-        </div>
-        <Input/>
-      </div>
+    <div className="flex flex-col items-center justify-center min-h-svh">
+          <Button onClick={()=>addUser()}>Add User</Button>
+     <p>apps/widget</p>
+     <div className='max-w-sm w-full mx-auto'>
+     {JSON.stringify(users, null , 2)}
+     </div>
     </div>
   )
 }
---
+++ packages/backend/convex/users.ts
@@ -0,0 +1,21 @@
+import { query, mutation } from "./_generated/server";
+
+export const getMany = query({
+  args: {},
+  handler: async (ctx) => {
+    const users = await ctx.db.query("users").collect();
+
+    return users;
+  },
+});
+
+export const add =mutation({
+  args: {},
+  handler: async (ctx) => {
+    const user = await ctx.db.insert("users", {
+      name: "harsh",
+    });
+
+    return user;
+  }
+})  
